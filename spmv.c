7ae2b212536a746fea372bca22b9996434580739
Emilio J. Padrón González <emilioj@udc.gal>
2024-10-03 13:08:47 +0200

    Refactor some stuff to improve code skeleton

--- spmv.c
+++ spmv.c
@@ -10,31 +10,34 @@
 #define DEFAULT_SIZE 1024
 #define DEFAULT_DENSITY 0.25
 
-double *generate_sparse_matrix(unsigned int seed, unsigned int size, double density)
+unsigned int populate_sparse_matrix(double mat[], unsigned int n, double density, unsigned int seed)
 {
-  srand(seed);
-  double *matrix = (double *) calloc(size * size, sizeof(double));
+  unsigned int nnz = 0;
 
-  for (unsigned int i = 0; i < size * size; i++) {
+  srand(seed);
+
+  for (unsigned int i = 0; i < n * n; i++) {
     if ((rand() % 100) / 100.0 < density) {
       // Get a pseudorandom value between -9.99 e 9.99
-      matrix[i] = ((double)(rand() % 10) + (double)rand() / RAND_MAX) * (rand() % 2 == 0 ? 1 : -1);
+      mat[i] = ((double)(rand() % 10) + (double)rand() / RAND_MAX) * (rand() % 2 == 0 ? 1 : -1);
+      nnz++;
+    } else {
+      mat[i] = 0;
     }
   }
 
-  return matrix;
+  return nnz;
 }
 
-double *generate_vector(unsigned int seed, unsigned int size)
+unsigned int populate_vector(double vec[], unsigned int size, unsigned int seed)
 {
   srand(seed);
-  double *vec = (double *) malloc(sizeof(double) * size);
 
   for (unsigned int i = 0; i < size; i++) {
     vec[i] = ((double)(rand() % 10) + (double)rand() / RAND_MAX) * (rand() % 2 == 0 ? 1 : -1);
   }
 
-  return vec;
+  return size;
 }
 
 int is_nearly_equal(double x, double y)
@@ -72,23 +75,29 @@ int main(int argc, char *argv[])
 
   double *mat, *vec, *refsol, *mysol;
 
-  mat = generate_sparse_matrix(1, size, density);
-  vec = generate_vector(2, size);
+  mat = (double *) malloc(size * size * sizeof(double));
+  vec = (double *) malloc(size * sizeof(double));
   refsol = (double *) malloc(size * sizeof(double));
   mysol = (double *) malloc(size * sizeof(double));
 
+  unsigned int nnz = populate_sparse_matrix(mat, size, density, 1);
+  populate_vector(vec, size, 2);
+
+  printf("Matriz size: %d x %d (%ld elements)\n", size, size, size*size);
+  printf("%d non-zero elements (%.2lf\%)\n\n", nnz, (double) nnz / (size*size) * 100.0);
 
   //
   // Dense computation using CBLAS (eg. GSL's CBLAS implementation)
   //
+  printf("Dense computation\n----------------\n");
+
   timeinfo start, now;
   timestamp(&start);
 
   cblas_dgemv(CblasRowMajor, CblasNoTrans, size, size, 1.0, mat, size, vec, 1, 0.0, refsol, 1);
 
   timestamp(&now);
-  printf("Time taken by Lapacke/OpenBLAS dense computation: %ld ms\n", diff_milli(&start, &now));
-
+  printf("Time taken by CBLAS dense computation: %ld ms\n", diff_milli(&start, &now));
 
   //
   // Using your own dense implementation
@@ -98,7 +107,7 @@ int main(int argc, char *argv[])
   my_dense(size, mat, vec, mysol);
 
   timestamp(&now);
-  printf("Time taken by my dense matrix - vector product: %ld ms\n", diff_milli(&start, &now));
+  printf("Time taken by my dense matrix-vector product: %ld ms\n", diff_milli(&start, &now));
 
   if (check_result(refsol, mysol, size) == 1)
     printf("Result is ok!\n");
