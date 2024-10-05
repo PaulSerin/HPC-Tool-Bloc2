0cbbb9de6259c25f5fdd1d444ea5579779c8e2e3
Paul SERIN <serinpaul1@gmail.com>
2024-10-05 16:58:46 +0200

    Update spmv.h

--- spmv.h
+++ spmv.h
@@ -1,2 +1,7 @@
-int my_dense(const unsigned int n, const double mat[], double vec[], double result[]);
-int my_sparse();
+#ifndef SPMV_H
+#define SPMV_H
+
+void my_dense(int size, const double mat[], const double vec[], double result[]);
+int my_sparse(int size, const double mat[], const double vec[], double result[]);
+
+#endif
