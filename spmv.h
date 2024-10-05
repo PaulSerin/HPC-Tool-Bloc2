f016026e38f326c498cdcc19857a18ceeac1752c
Paul SERIN <serinpaul1@gmail.com>
2024-10-05 17:49:46 +0200

    update spmv.h with all the functions and CSR struct

--- spmv.h
+++ spmv.h
@@ -1,7 +1,18 @@
 #ifndef SPMV_H
 #define SPMV_H
 
-void my_dense(int size, const double mat[], const double vec[], double result[]);
-int my_sparse(int size, const double mat[], const double vec[], double result[]);
+int my_dense(const unsigned int n, const double mat[], double vec[], double result[]);
+
+typedef struct {
+    int *row_ptr;     // Pointer to the start of each row
+    int *col_indices; // Column indices of the non-zero values
+    double *values;   // Non-zero values of the matrix
+} CSRMatrix;
+
+CSRMatrix* dense_to_csr(const unsigned int n, const double mat[]);
+
+int my_sparse(const CSRMatrix *csr, const double vec[], double result[], unsigned int n);
+
+void free_csr(CSRMatrix *csr);
 
 #endif
